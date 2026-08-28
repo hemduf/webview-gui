8b27eb01a86af9800b06a9a998501dfbd641d01e
hemduf <fduhem@gmail.com>
2026-08-28 17:37:25 +0200

    feat(wclap): select WebView-only backend for WASM

--- include/webview-gui/_impl/webview-gui.hxx
+++ include/webview-gui/_impl/webview-gui.hxx
@@ -1,7 +1,7 @@
 #pragma once
 
-#if defined(__EMSCRIPTEN__) || defined(__wasm__) || defined(__wasm32__) || defined(__wasm64__)
-#	include "./platform/not-supported.h"
+#if defined(WEBVIEW_GUI_WEBVIEW_ONLY) || defined(__EMSCRIPTEN__) || defined(__wasm__) || defined(__wasm32__) || defined(__wasm64__)
+#	include "./platform/webview-only.h"
 #elif defined(__has_include) && (__has_include("choc/gui/choc_WebView.h") || __has_include("./platform/choc/gui/choc_WebView.h"))
 #	include "./platform/choc.h"
 #else
