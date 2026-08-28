12039d20f46a9553a11b7b7c313fcb6fc1329b2e
hemduf <fduhem@gmail.com>
2026-08-28 18:56:07 +0200

    test(clap): compile adapter with canonical SDK webview header

---
+++ tests/clap_compat_consumer/current_compile.cpp
@@ -0,0 +1,19 @@
+#include "webview-gui/clap-webview-gui.h"
+
+#include <cstring>
+#include <type_traits>
+
+static_assert(std::is_same<webview_gui::clap_plugin_webview,
+                           clap_plugin_webview_t>::value,
+              "SDK plugin WebView ABI must remain canonical");
+static_assert(std::is_same<webview_gui::clap_host_webview,
+                           clap_host_webview_t>::value,
+              "SDK host WebView ABI must remain canonical");
+
+int main()
+{
+    return std::strcmp(webview_gui::CLAP_EXT_WEBVIEW, "clap.webview/3") == 0
+               && std::strcmp(webview_gui::CLAP_WINDOW_API_WEBVIEW, "webview") == 0
+           ? 0
+           : 1;
+}
