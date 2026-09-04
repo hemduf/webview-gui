d397080fa41d87e40bce87830e083b982183dceb
hemduf <fduhem@gmail.com>
2026-09-04 11:41:21 +0200

    refactor(polysynth): inject preset catalog per instance

--- examples/polysynth/polysynth_plugin.h
+++ examples/polysynth/polysynth_plugin.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../common/example_plugin_ids.h"
+#include "../common/preset_catalog_factory.h"
 
 #include <clap/clap.h>
 
@@ -13,6 +14,9 @@ inline constexpr clap_id kPolySynthAudioOutputPortId = 0x3000u;
 inline constexpr clap_id kPolySynthNoteInputPortId = 0x3100u;
 inline constexpr std::size_t kPolySynthDefaultVoiceCount = 16u;
 
+const clap_plugin_t *createPolySynthPlugin(
+    const clap_host_t *host,
+    presets::PresetCatalogFactory presetCatalogFactory) noexcept;
 const clap_plugin_factory_t *polysynthFactory() noexcept;
 
 } // namespace webview_gui::examples::polysynth
